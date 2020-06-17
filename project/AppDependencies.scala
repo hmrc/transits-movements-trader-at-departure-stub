bda67d7340ff780b1863641f6738f337b188c88f
crystalmark <tim@crystalmark.co.uk>
2020-06-17 13:21:41 +0100

    CTDA-273 Added POST endpoint for IE015

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,16 +6,14 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-
-    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.8.0"
+    "uk.gov.hmrc"       %% "bootstrap-play-26"             % "1.7.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.8.0" % Test classifier "tests",
     "org.scalatest"           %% "scalatest"                % "3.0.8"                 % "test",
     "com.typesafe.play"       %% "play-test"                % current                 % "test",
-    "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "3.1.2"                 % "test, it"
+    "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test",
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "3.1.2"                 % "test",
+    "org.mockito"             %  "mockito-all"              % "1.10.19"
   )
-
 }
